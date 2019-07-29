15390cedd8342e488ab003bc4f95fee65f9d74d2
Saleedher Muraleedharan <Saleedher.Muraleedha@gds.ey.com>
2019-07-29 12:52:04 +0530

    Initial Commit - SF gateway

---
+++ Gateway/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using C3.ServiceFabric.HttpServiceGateway;
+using C3.ServiceFabric.HttpCommunication;
+
+namespace Gateway
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddServiceFabricHttpCommunication();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            //This key value can be configured in database and we can facilate UI to manage this key value.   
+            //app.RunHttpServiceGateway("/SFGateway", "fabric:/MyApp/WebService");
+            app.RunHttpServiceGateway("/api/uploadFile", "fabric:/HelloWorldStateless/HelloWorldStatelessService");
+
+            // catch-all
+            app.Run(async context =>
+            {
+                var logger = loggerFactory.CreateLogger("Catch-All");
+                logger.LogWarning("No endpoint found for request {path}", context.Request.Path + context.Request.QueryString);
+
+                context.Response.StatusCode = (int)System.Net.HttpStatusCode.NotFound;
+                await context.Response.WriteAsync("Not Found");
+            });
+        }
+    }
+}
